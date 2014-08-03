e97c566b66d3e92ca076c78fc53cb08cec255a3c
jerkey <jerkey@halo.nu>
2014-08-03 00:00:55 -0700

    just use the hardware serial port for both things, very simple.

--- yasnac_fakery.ino
+++ yasnac_fakery.ino
@@ -1,49 +1,26 @@
-/*
- Not all pins on the Mega and Mega 2560 support change interrupts, 
- so only the following can be used for RX: 
- 10, 11, 12, 13, 50, 51, 52, 53, 62, 63, 64, 65, 66, 67, 68, 69
- 
- Not all pins on the Leonardo support change interrupts, 
- so only the following can be used for RX: 
- 8, 9, 10, 11, 14 (MISO), 15 (SCK), 16 (MOSI).
- 
- by jerquee based on a snippet by Tom Igoe
- based on Mikal Hart's example
- This example code is in the public domain.
- */
-#include <SoftwareSerial.h>
+#define CN1_SEN 8 // goes high when it wants serial data
+#define CN2_SEN 9 // goes high when it wants serial data
 
-#define CN1_SEN 9 // send out the serial after this goes high
-#define CN2_SEN 8
-
-boolean cn1_sent = false;
-boolean cn2_sent = false;
-
-SoftwareSerial cn1(10, 11); // RX, TX
-SoftwareSerial cn2(13, 12); // RX, TX
+boolean cn1_sent = false; // whether we have sent data for this one yet
+boolean cn2_sent = false; // whether we have sent data for this one yet
 
 void setup()  
 {
-  // Open serial communications and wait for port to open:
-//  Serial.begin(57600);
-//  while (!Serial) {
-//    ; // wait for serial port to connect. Needed for Leonardo only
-//  }
-
-  // set the data rate for the SoftwareSerial port
-  cn1.begin(9600);
-  cn2.begin(9600);
+  Serial.begin(9600,SERIAL_7E1);
+  delay(1000);
 }
 
-void loop() // run over and over
+void loop()
 {
   if (digitalRead(CN1_SEN) && !cn1_sent) {
-    cn1.write("P+00000\r");
+    Serial.print("P+00000\r");
     cn1_sent = true;
     }
+  if (!digitalRead(CN1_SEN)) cn1_sent = false;
   if (digitalRead(CN2_SEN) && !cn2_sent) {
-    cn2.write("P+00000\r");
+    Serial.print("P+00000\r");
     cn2_sent = true;
     }
+  if (!digitalRead(CN2_SEN)) cn2_sent = false;
 }
 
