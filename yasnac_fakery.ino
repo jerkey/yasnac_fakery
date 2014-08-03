f67764f33244a49c27351242f514513e4cb826df
jerkey <jerkey@halo.nu>
2014-08-02 23:51:55 -0700

    written the way it should be, except that we need E71 and softwareSerial only does N81

--- yasnac_fakery.ino
+++ yasnac_fakery.ino
@@ -1,14 +1,4 @@
 /*
-  Software serial multple serial test
- 
- Receives from the hardware serial, sends to software serial.
- Receives from software serial, sends to hardware serial.
- 
- The circuit: 
- * RX is digital pin 10 (connect to TX of other device)
- * TX is digital pin 11 (connect to RX of other device)
- 
- Note:
  Not all pins on the Mega and Mega 2560 support change interrupts, 
  so only the following can be used for RX: 
  10, 11, 12, 13, 50, 51, 52, 53, 62, 63, 64, 65, 66, 67, 68, 69
@@ -17,39 +7,43 @@
  so only the following can be used for RX: 
  8, 9, 10, 11, 14 (MISO), 15 (SCK), 16 (MOSI).
  
- created back in the mists of time
- modified 25 May 2012
- by Tom Igoe
+ by jerquee based on a snippet by Tom Igoe
  based on Mikal Hart's example
- 
  This example code is in the public domain.
- 
  */
 #include <SoftwareSerial.h>
 
-SoftwareSerial mySerial(10, 11); // RX, TX
+#define CN1_SEN 9 // send out the serial after this goes high
+#define CN2_SEN 8
+
+boolean cn1_sent = false;
+boolean cn2_sent = false;
+
+SoftwareSerial cn1(10, 11); // RX, TX
+SoftwareSerial cn2(13, 12); // RX, TX
 
 void setup()  
 {
   // Open serial communications and wait for port to open:
-  Serial.begin(57600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for Leonardo only
-  }
-
-
-  Serial.println("Goodnight moon!");
+//  Serial.begin(57600);
+//  while (!Serial) {
+//    ; // wait for serial port to connect. Needed for Leonardo only
+//  }
 
   // set the data rate for the SoftwareSerial port
-  mySerial.begin(4800);
-  mySerial.println("Hello, world?");
+  cn1.begin(9600);
+  cn2.begin(9600);
 }
 
 void loop() // run over and over
 {
-  if (mySerial.available())
-    Serial.write(mySerial.read());
-  if (Serial.available())
-    mySerial.write(Serial.read());
+  if (digitalRead(CN1_SEN) && !cn1_sent) {
+    cn1.write("P+00000\r");
+    cn1_sent = true;
+    }
+  if (digitalRead(CN2_SEN) && !cn2_sent) {
+    cn2.write("P+00000\r");
+    cn2_sent = true;
+    }
 }
 
