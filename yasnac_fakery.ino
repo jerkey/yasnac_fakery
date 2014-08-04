f395a7a8508a4fb5e48fc117fded8d0fe67023e0
jerkey <jerkey@halo.nu>
2014-08-03 18:34:23 -0700

    modified for seeeduino mega, with two serial ports

--- yasnac_fakery.ino
+++ yasnac_fakery.ino
@@ -1,62 +1,66 @@
-#define CN1_SEN 8 // goes high when it wants serial data
-#define CN2_SEN 9 // goes high when it wants serial data
-#define CN1_PB 10 // PB pin
-#define CN2_PB 11 // PB pin
+#define CN1_SEN 2 // goes high when it wants serial data
+#define CN1_PA 18 // this is the same as Serial1 TX pin (must be 18)
+#define CN1_PB  3 // PB pin
+
+#define CN2_SEN 4 // goes high when it wants serial data
+#define CN2_PA 16 // this is the same as Serial2 TX pin (must be 16)
+#define CN2_PB  5 // PB pin
 
 boolean cn1_sent = false; // whether we have sent data for this one yet
 boolean cn2_sent = false; // whether we have sent data for this one yet
 
 void setup()  
 {
-  Serial.begin(9600,SERIAL_7E1);
+  Serial.begin(9600); // ,SERIAL_7E1);
+  Serial.println("i lie to YASNACs");
   delay(1000);
 }
 
 void loop()
 {
   if (digitalRead(CN1_SEN) && !cn1_sent) {
-    Serial.begin(9600,SERIAL_7E1);
+    Serial1.begin(9600,SERIAL_7E1);
     delay(185);
-    Serial.print("P");
+    Serial1.print("P");
     delay(2);
-    Serial.print("+");
+    Serial1.print("+");
     delay(2);
-    Serial.print("0");
+    Serial1.print("0");
     delay(2);
-    Serial.print("0");
+    Serial1.print("0");
     delay(2);
-    Serial.print("0");
+    Serial1.print("0");
     delay(2);
-    Serial.print("0");
+    Serial1.print("0");
     delay(2);
-    Serial.print("0");
+    Serial1.print("0");
     delay(2);
-    Serial.print("\r");
+    Serial1.print("\r");
     delayMicroseconds(2470);
-    sendIncrementalPulses(1,CN1_PB);
+    sendIncrementalPulses(CN1_PA,CN1_PB);
     cn1_sent = true;
     }
   if (!digitalRead(CN1_SEN)) cn1_sent = false;
   if (digitalRead(CN2_SEN) && !cn2_sent) {
-    Serial.begin(9600,SERIAL_7E1);
+    Serial2.begin(9600,SERIAL_7E1);
     delay(185);
-    Serial.print("P");
+    Serial2.print("P");
     delay(2);
-    Serial.print("+");
+    Serial2.print("+");
     delay(2);
-    Serial.print("0");
+    Serial2.print("0");
     delay(2);
-    Serial.print("0");
+    Serial2.print("0");
     delay(2);
-    Serial.print("0");
+    Serial2.print("0");
     delay(2);
-    Serial.print("0");
+    Serial2.print("0");
     delay(2);
-    Serial.print("0");
+    Serial2.print("0");
     delay(2);
-    Serial.print("\r");
+    Serial2.print("\r");
     delayMicroseconds(2470);
-    sendIncrementalPulses(1,CN2_PB);
+    sendIncrementalPulses(CN2_PA,CN2_PB);
     cn2_sent = true;
     }
   if (!digitalRead(CN2_SEN)) cn2_sent = false;
