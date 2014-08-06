93418a931a715661a28b26822deaefac1286202d
jerkey <jerkey@halo.nu>
2014-08-06 01:37:57 -0700

    rewritten sendIncrementalPulses() based on login analyzer data to EXACTLY copy the real encoder

--- yasnac_fakery.ino
+++ yasnac_fakery.ino
@@ -6,13 +6,21 @@
 #define CN2_PA 16 // this is the same as Serial2 TX pin (must be 16)
 #define CN2_PB  5 // PB pin
 
+#define TUNED43US 38 // how many "delayMicroseconds" to achieve 43uS actual time
+
 boolean cn1_sent = false; // whether we have sent data for this one yet
 boolean cn2_sent = false; // whether we have sent data for this one yet
+
+int state; // binary state of pin1 (bit0) and pin2 (bit1)
 
 void setup()  
 {
   Serial.begin(9600); // ,SERIAL_7E1);
   Serial.println("i lie to YASNACs");
+  digitalWrite(CN1_PB,HIGH);
+  digitalWrite(CN2_PB,HIGH);
+  pinMode(CN1_PB,OUTPUT);
+  pinMode(CN2_PB,OUTPUT);
   delay(1000);
 }
 
@@ -36,7 +44,8 @@ void loop()
     Serial1.print("0");
     delay(2);
     Serial1.print("\r");
-    delayMicroseconds(2470);
+    delayMicroseconds(2470+693);
+    UCSR1B &= (255 - (1<<TXEN1)); // change USART1 pin to be a GPIO again
     sendIncrementalPulses(CN1_PA,CN1_PB);
     cn1_sent = true;
     }
@@ -59,7 +68,8 @@ void loop()
     Serial2.print("0");
     delay(2);
     Serial2.print("\r");
-    delayMicroseconds(2470);
+    delayMicroseconds(2470+693);
+    UCSR2B &= (255 - (1<<TXEN2)); // change USART2 pin to be a GPIO again
     sendIncrementalPulses(CN2_PA,CN2_PB);
     cn2_sent = true;
     }
@@ -67,16 +77,28 @@ void loop()
 }
 
 void sendIncrementalPulses(int pin1, int pin2) {
-  pinMode(pin1,OUTPUT);
-  pinMode(pin2,OUTPUT);
-  for (int i = 0; i < 10; i++) {
-    digitalWrite(pin1,HIGH);
-    delayMicroseconds(500);
-    digitalWrite(pin2,HIGH);
-    delayMicroseconds(500);
-    digitalWrite(pin1,LOW);
-    delayMicroseconds(500);
-    digitalWrite(pin2,LOW);
-    delayMicroseconds(500);
+  state = 2; // binary state of pin1 (bit0) and pin2 (bit1)
+  pulseSeq(30,pin1,pin2); // first
+  pulseSeq(30,pin1,pin2); // second
+  pulseSeq(30,pin1,pin2); // third
+  pulseSeq(62,pin1,pin2); // fourth
+  pulseSeq(30,pin1,pin2); // fifth
+  pulseSeq(30,pin1,pin2); // sixth
+  pulseSeq(62,pin1,pin2); // seventh
+  pulseSeq(30,pin1,pin2); // eighth
+  pulseSeq(20,pin1,pin2); // last time
+} // the above numbers were based on spreadsheet analysys of realencoder-notes.ods and logicdata
+
+void pulseSeq(int count, int pin1, int pin2) {
+  for (int i = 0; i < count; i++) {
+    digitalWrite(pin1,state & 1);
+    digitalWrite(pin2,state & 2);
+    delayMicroseconds(TUNED43US);
+    state += 1;
   }
+  digitalWrite(pin1,state & 1);
+  digitalWrite(pin2,state & 2);
+  delayMicroseconds(TUNED43US);
+  delayMicroseconds(TUNED43US); // this is a double-length state
+  state += 1;
 }
